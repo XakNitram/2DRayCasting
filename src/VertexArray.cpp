13c32d9fff26210e9d859186db5436d7ce8ae8ff
Tate Mioton <tatemioton@outlook.com>
2020-07-29 17:13:21 -0500

    Implement Attaching an Index Buffer to a VertexArray Object.

--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -1,47 +1,128 @@
-#include <iostream>
-#include "GLUtils.h"
 #include "VertexArray.h"
 
 
-VertexArray::VertexArray(): attributeCount(0) {
+void VertexArray::setup() {
 	GLCall(glGenVertexArrays(1, &id));
 	GLCall(glBindVertexArray(id));
 	GLCall(glGenBuffers(1, &vbo));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
 }
 
-VertexArray::VertexArray(VertexArray&& other) noexcept : id(other.id), vbo(other.vbo), attributeCount(0) {
+void VertexArray::genIndexBuffer() {
+#ifdef _DEBUG
+	std::cout << "Initializing element buffer on vertex array " << id << " post-initialization." << std::endl;
+#endif // _DEBUG
+
+	glBindVertexArray(id);
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+}
+
+VertexArray::VertexArray(): ebo(0), attributeCount(0) {
+	setup();
+}
+
+VertexArray::VertexArray(bool useElementsBuffer): ebo(0), attributeCount(0) {
+	setup();
+	
+	if (useElementsBuffer) {
+		GLCall(glGenBuffers(1, &ebo));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo));
+	}
+}
+
+VertexArray::VertexArray(VertexArray&& other) noexcept : id(other.id), vbo(other.vbo), ebo(other.ebo), attributeCount(other.attributeCount) {
+#ifdef _DEBUG
+	std::cout << "Moving vertex array " << id << '.' << std::endl;
+#endif // _DEBUG
+
+
 	other.id = 0;
 	other.vbo = 0;
+	other.ebo = 0;
 }
 
 VertexArray::~VertexArray() {
 	if (id) {
+		if (ebo) {
+			GLCall(glDeleteBuffers(1, &ebo));
+		}
 		GLCall(glDeleteBuffers(1, &vbo));
 		GLCall(glDeleteVertexArrays(1, &id));
+
+#ifdef _DEBUG
+		std::cout << "Destroying vertex array (" << id << ") and associated buffers." << std::endl;
+#endif // DEBUG
 	}
+
+#ifdef _DEBUG
+	else {
+		std::cout << "Vertex array lifetime ended with an id of 0." << std::endl;
+	}
+#endif // DEBUG
 }
 
-void VertexArray::constructBuffer(GLsizei size, const void* data, GLenum usage) {
+void VertexArray::constructArrayBuffer(GLsizei size, const void* data, GLenum usage) {
+#ifdef _DEBUG
+	std::cout << "Constructing array buffer of " << size << " bytes on vertex array " << id << '.' << std::endl;
+#endif // DEBUG
 	GLCall(glBindVertexArray(id));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
 	GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, usage));
 }
 
-void VertexArray::setData(GLintptr offset, GLsizeiptr size, const void* data) {
+void VertexArray::setArrayData(GLintptr offset, GLsizeiptr size, const void* data) {
+#ifdef _DEBUG
+	std::cout << "Updating array buffer with" << size << "bytes of data." << std::endl;
+#endif // _DEBUG
+
 	GLCall(glBindVertexArray(id));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
 	GLCall(glBufferSubData(GL_ARRAY_BUFFER, offset, size, data));
 }
 
+void VertexArray::constructIndexBuffer(GLsizei size, const void* data, GLenum usage) {
+#ifdef _DEBUG
+	std::cout << "Constructing element buffer of " << size << " bytes on vertex array " << id << '.' << std::endl;
+#endif // _DEBUG
+	if (!ebo) {
+		genIndexBuffer();
+	}
+	
+	else {
+		GLCall(glBindVertexArray(id));
+	}
+	
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, usage));
+}
+
+void VertexArray::setIndexData(GLintptr offset, GLsizeiptr size, const void* data) {
+#ifdef _DEBUG
+	std::cout << "Updating element buffer with " << size << " bytes of data." << std::endl;
+#endif // _DEBUG
+	GLCall(glBindVertexArray(id));
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, offset, size, data);
+}
+
 void VertexArray::attachAttribute(GLuint dimensions, GLenum type, GLsizei size) {
+#ifdef _DEBUG
+	std::cout << "Attaching " << dimensions << "d attribute"
+		<< ", " << "location=" << attributeCount
+		<< " (" << size << " bytes)"
+		<< " on vertex array " << id << '.'
+		<< std::endl;
+#endif // DEBUG
 	GLCall(glBindVertexArray(id));
 	GLCall(glEnableVertexArrayAttrib(id, attributeCount));
 	GLCall(glVertexAttribPointer(attributeCount, dimensions, type, GL_FALSE, size, nullptr));
 	attributeCount++;
 }
 
-void VertexArray::draw(GLenum mode, GLuint count) const {
+void VertexArray::drawArrays(GLenum mode, GLuint count) const {
 	GLCall(glBindVertexArray(id));
 	GLCall(glDrawArrays(mode, 0, count));
+}
+
+void VertexArray::drawElements(GLenum mode, GLuint count, GLenum type) const {
+	ASSERT(ebo);
+	GLCall(glBindVertexArray(id));
+	GLCall(glDrawElements(mode, count, type, nullptr));
 }
