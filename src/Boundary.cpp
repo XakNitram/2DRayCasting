93b48d3d69dd5f1a3f33050278f4f6573e3ef8d9
Tate Mioton <tatemioton@outlook.com>
2020-08-01 15:59:17 -0500

    Add Boundary Move-Constructor. Fix Boundary Constructor.

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -1,12 +1,14 @@
 #include "Boundary.h"
 
-Boundary::Boundary(float x1, float y1, float x2, float y2): line(x1, y1, x2, y1), vao(true) {
+Boundary::Boundary(float x1, float y1, float x2, float y2): line(x1, y1, x2, y2), vao(true) {
 	float positions[4] = { x1, y1, x2, y2 };
 
 	// upload vertex position data
 	vao.constructArrayBuffer(sizeof(positions), positions, GL_STATIC_DRAW);
 	vao.attachAttribute(2, GL_FLOAT, 2 * sizeof(float));
 }
+
+Boundary::Boundary(Boundary&& other) noexcept: line(other.line), vao(std::move(other.vao)) {}
 
 void Boundary::draw() const {
 	vao.drawArrays(GL_LINES, 2);
--- src/Boundary.h
+++ src/Boundary.h
@@ -13,6 +13,7 @@ struct Boundary {
 
 	// TODO: if needed, add a default constructor and an 'update' method
 	Boundary(float x1, float y1, float x2, float y2);
+	Boundary(Boundary&& other) noexcept;
 
 	void draw() const;
 };
