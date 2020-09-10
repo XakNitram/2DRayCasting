039f446b486006d0685d1c6e7b564e92bf6b40d9
Tate Mioton <tatemioton@outlook.com>
2020-09-10 01:26:52 -0500

    Change Project Layout

--- res/Shaders/default.frag
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-layout(location = 0) out vec4 color;
-uniform vec3 u_Color = vec3(0.2, 0.4, 0.8);
-
-void main() {
-    color = vec4(u_Color, 1.0);
-}
--- src/Utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <fstream>
-#include <sstream>
-
-#define ASSERT(x) if (!(x)) __debugbreak();
-
-std::string readFile(const std::string & filepath);
