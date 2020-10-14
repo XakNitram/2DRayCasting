60a20d13609b986fe7f7681984c22d2ec6036ebb
Tate Mioton <tatemioton@outlook.com>
2020-10-14 02:40:37 -0500

    Implement Shadows and Light Falloff

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,6 +8,8 @@
 #include "Math/Boundary.h"
 #include "Casters/AngleCaster.h"
 #include "Casters/EndPointCaster.h"
+#include "Primitives/Quad.h"
+#include "Primitives/Floor.h"
 
 // Code Signing: https://stackoverflow.com/questions/16673086/how-to-correctly-sign-an-executable/48244156
 
@@ -47,26 +49,6 @@ struct SimulationConfig {
 };
 
 
-// Uploading time data to shader:
-class ShaderTime {
-	Uniform m_uniform;
-	double m_start;
-
-public:
-	ShaderTime() : m_start(0.0) {}
-
-	void configure(ShaderProgram& program, const std::string& name) {
-		m_uniform = program.uniform(name);
-	}
-
-	void set(double time) {
-		m_uniform.set1f(static_cast<float>(time - m_start));
-	}
-
-	Uniform& uniform() { return m_uniform; }
-};
-
-
 class RayCasting final : public Simulation {
 protected:
 	void handleKeys(int key, int scancode, int action, int mods) final {
@@ -101,19 +83,24 @@ protected:
 
 private:
 	SimulationConfig settings;
-	ShaderProgram lineShader, lightShader;
+	GLuint floorTexture;
+	ShaderProgram lineShader, lightShader, floorShader;
 	std::vector<Boundary> bounds;
 	CasterConfig casters[4];
-	//ShaderTime timer;
+	std::unique_ptr<Floor> floor;
+	Uniform lightCenter;
 
 	inline std::unique_ptr<Caster>& currentCaster() {
 		return casters[static_cast<size_t>(settings.mode)].caster;
 	}
 
 public:
-	RayCasting(unsigned int width, unsigned int height) : Simulation(width, height, "RayCasting") {
+	~RayCasting() final { glDeleteTextures(1, &floorTexture); }
+	RayCasting(unsigned int width, unsigned int height, GLFWmonitor* monitor = nullptr) : Simulation(width, height, "RayCasting", monitor) {
 		attachKeyCallback();
-		setSwapInterval(1);
+		swapInterval(1);
+
+		//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
 		// ****** Set Up OpenGL Stuff ******
 		glEnable(GL_BLEND);
@@ -128,14 +115,57 @@ public:
 		settings.frameWidth  = frameWidth;
 		settings.frameHeight = frameHeight;
 
-		// ****** Construct Shaders ******
-		const VertexShader vertex(readFile("Shaders/default.vert"));
-		FragmentShader fragment(readFile("Shaders/default.frag"));
-		lineShader.link(vertex, fragment);
+		const float wPad = (10.0f * frameWidth) / 800.0f;
+		const float hPad = (10.0f * frameHeight) / 600.0f;
 
-		//fragment = FragmentShader(readFile("Shaders/spacetime.frag"));
-		//fragment = FragmentShader(readFile("Shaders/mazing.frag"));
-		lightShader.link(vertex, fragment);
+		float floorTextureWidth = frameWidth - (2.0f * wPad);
+		float floorTextureHeight = frameHeight - (2.0f * hPad);
+
+		floor = std::make_unique<Floor>(wPad, hPad, floorTextureWidth, floorTextureHeight);
+
+		// ****** Render Floor Texture ******
+		glGenTextures(1, &floorTexture); {
+			GLsizei ftwi = static_cast<GLsizei>(floorTextureWidth);
+			GLsizei fthi = static_cast<GLsizei>(floorTextureHeight);
+			GLuint floorFrame;
+			glGenFramebuffers(1, &floorFrame);
+			glBindFramebuffer(GL_FRAMEBUFFER, floorFrame);
+
+			glBindTexture(GL_TEXTURE_2D, floorTexture);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ftwi, fthi, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, floorTexture, 0);
+
+			Quad textureBase(-1.0f, -1.0f, 2.0f, 2.0f);
+
+			ShaderProgram floorTexturePipeline;
+			VertexShader floorVertex("#version 330 core\nlayout(location=0) in vec4 position;\nvoid main() { gl_Position = position; }");
+			//FragmentShader floorFragment(readFile("Shaders/default.frag"));
+			FragmentShader floorFragment(readFile("Shaders/mazing.frag"));
+			floorTexturePipeline.link(floorVertex, floorFragment);
+
+			floorTexturePipeline.bind();
+			//floorTexturePipeline.uniform("u_Color").set3f(1.0f, 0.0f, 0.0f);
+			floorTexturePipeline.uniform("u_Resolution").set2f(floorTextureWidth, floorTextureHeight);
+
+			GLsizei prevViewport[4];
+			glGetIntegerv(GL_VIEWPORT, prevViewport);
+			glViewport(0, 0, ftwi, fthi);
+			textureBase.draw();
+			glViewport(0, 0, prevViewport[2], prevViewport[3]);
+
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			glDeleteFramebuffers(1, &floorFrame);
+		}
+
+		// ****** Construct Shaders ******
+		lineShader.link(readFile("Shaders/default.vert"), readFile("Shaders/default.frag"));
+		lightShader.link(readFile("Shaders/light.vert"), readFile("Shaders/light.frag"));
+		//lightShader.link(readFile("Shaders/light.vert"), readFile("Shaders/floor.frag"));
+		floorShader.link(readFile("Shaders/floor.vert"), readFile("Shaders/floor.frag"));
 
 		// ****** Initialize Shader Uniforms ******
 		lineShader.bind();
@@ -144,16 +174,17 @@ public:
 
 		lightShader.bind();
 		lightShader.uniform("u_Projection").set2DOrthographic(frameHeight, 0.0f, frameWidth, 0.0f);
-		//lightShader.uniform("u_Color").set3f(1.0f, 0.0f, 0.0f);
-		//lightShader.uniform("u_Resolution").set2f(frameWidth, frameHeight);
-		//timer.configure(lightShader, "u_Time");
-		//timer.set(glfwGetTime());
+		lightShader.uniform("u_Resolution").set2f(floorTextureWidth, floorTextureHeight);
+		lightShader.uniform("u_Offset").set2f(wPad, hPad);
+		lightShader.uniform("u_Texture").set1i(0);
+		lightCenter = lightShader.uniform("u_MouseCoords");
+
+		floorShader.bind();
+		floorShader.uniform("u_Projection").set2DOrthographic(frameHeight, 0.0f, frameWidth, 0.0f);
+		floorShader.uniform("u_Texture").set1i(0);
 
 		// ****** Construct Boundaries ******
-		bounds.reserve(16); {
-			const float wPad = (10.0f * frameWidth) / 400.0f;
-			const float hPad = (10.0f * frameHeight) / 400.0f;
-
+		bounds.reserve(14); {
 			bounds.emplace_back(wPad, hPad, frameWidth - wPad, hPad);
 			bounds.emplace_back(frameWidth - wPad, hPad, frameWidth - wPad, frameHeight - hPad);
 			bounds.emplace_back(frameWidth - wPad, frameHeight - hPad, wPad, frameHeight - hPad);
@@ -165,14 +196,14 @@ public:
 			const float height34 = 3.0f * frameHeight / 4.0f;
 
 			bounds.emplace_back(width14 - wPad, height14 - hPad, width14 + wPad, height14 - hPad);
-			bounds.emplace_back(width14 + wPad, height14 - hPad, width14 + wPad, height34 + hPad);
+			//bounds.emplace_back(width14 + wPad, height14 - hPad, width14 + wPad, height34 + hPad);
 			bounds.emplace_back(width14 + wPad, height34 + hPad, width14 - wPad, height34 + hPad);
 			bounds.emplace_back(width14 - wPad, height34 + hPad, width14 - wPad, height14 - hPad);
 
 			bounds.emplace_back(width34 - wPad, height14 - hPad, width34 + wPad, height14 - hPad);
 			bounds.emplace_back(width34 + wPad, height14 - hPad, width34 + wPad, height34 + hPad);
 			bounds.emplace_back(width34 + wPad, height34 + hPad, width34 - wPad, height34 + hPad);
-			bounds.emplace_back(width34 - wPad, height34 + hPad, width34 - wPad, height14 - hPad);
+			//bounds.emplace_back(width34 - wPad, height34 + hPad, width34 - wPad, height14 - hPad);
 
 			const float wPad5 = 5.0f * wPad;
 			const float hPad5 = 5.0f * hPad;
@@ -187,9 +218,9 @@ public:
 		// ****** Initialize Casters ******
 		const unsigned int numBounds = bounds.size();
 		casters[static_cast<size_t>(RenderMode::FILLED_END_POINT)].setCaster(std::make_unique<FilledEndPointCaster>(numBounds));
-		casters[static_cast<size_t>(RenderMode::LINE_END_POINT)  ].setCaster(std::make_unique<EndPointCaster>(numBounds));
+		casters[static_cast<size_t>(RenderMode::LINE_END_POINT)  ].setCaster(std::make_unique<LineEndPointCaster>(numBounds));
 		casters[static_cast<size_t>(RenderMode::FILLED_ANGLE)    ].setCaster(std::make_unique<FilledAngleCaster>());
-		casters[static_cast<size_t>(RenderMode::LINE_ANGLE)      ].setCaster(std::make_unique<AngleCaster>());
+		casters[static_cast<size_t>(RenderMode::LINE_ANGLE)      ].setCaster(std::make_unique<LineAngleCaster>());
 	}
 
 	void update(double dt) final {
@@ -210,6 +241,8 @@ public:
 
 			if (!(mousePosXf == mode.prevX && mousePosYf == mode.prevY)) {
 				if (!(mousePosXf < wPad || mousePosXf > frameWidth - wPad || mousePosYf < hPad || mousePosYf > frameHeight - hPad)) {
+					lightShader.bind();
+					lightCenter.set2f(mousePosXf, frameHeight - mousePosYf);
 					entity->update(mousePosXf, frameHeight - mousePosYf);
 					entity->look(bounds);
 				}
@@ -228,8 +261,12 @@ public:
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Draw caster.
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		floorShader.bind();
+		floor->draw();
+
 		lightShader.bind();
-		//timer.set(glfwGetTime());
 		entity->draw();
 
 		if (settings.config & SHOW_BOUNDS) {
@@ -244,7 +281,16 @@ public:
 
 int main() {
 	try {
-		RayCasting sim(800, 600);
+		RayCasting::initGLFW();
+		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+		glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+		glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+		glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+		glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+		RayCasting sim(mode->width, mode->height, monitor);
 		sim.run();
 
 		return 0;
