831c4be72d29eac850b883248daaa8f8506ceb5a
Tate Mioton <tatemioton@outlook.com>
2021-01-27 03:47:08 -0600

    Abstract Floor Texture

--- src/Application.cpp
+++ src/Application.cpp
@@ -49,11 +49,77 @@ struct SimulationConfig {
 };
 
 
+class FloorTexture {
+    uint32_t m_id = 0;
+    uint32_t m_slot = 0;
+
+public:
+    FloorTexture() {
+        glGenTextures(1, &m_id);
+    }
+
+    ~FloorTexture() {
+        glDeleteTextures(1, &m_id);
+    }
+
+    void render(uint32_t width, uint32_t height) {
+        GLuint frame;
+        glGenFramebuffers(1, &frame);
+        glBindFramebuffer(GL_FRAMEBUFFER, frame);
+
+        glBindTexture(GL_TEXTURE_2D, m_id);
+        glTexImage2D(
+                GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                0, GL_RGB, GL_UNSIGNED_BYTE, nullptr
+        );
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_id, 0);
+
+        Quad floor(-1.0f, -1.0f, 2.0f, 2.0f);
+        lwvl::ShaderProgram texturePipeline;
+        lwvl::VertexShader fv("#version 330 core\nlayout(location=0) in vec4 position;\nvoid main() { gl_Position = position; }");
+        lwvl::FragmentShader ff(readFile("Shaders/default.frag"));
+//        lwvl::FragmentShader ff(readFile("Shaders/mazing.frag"));
+        texturePipeline.link(fv, ff);
+        texturePipeline.bind();
+
+        // Floor color:
+        texturePipeline.uniform("u_Color").set3f(1.00000f, 1.00000f, 1.00000f);  // White
+//        texturePipeline.uniform("u_Color").set3f(0.61569f, 0.63529f, 0.67059f);  // Silver
+
+        // resolution for mazing.frag
+//        texturePipeline.uniform("u_Resolution").set2f(static_cast<float>(width), static_cast<float>(height));
+
+        GLsizei prevViewport[4];
+        glGetIntegerv(GL_VIEWPORT, prevViewport);
+        const GLsizei prevWidth = prevViewport[2];
+        const GLsizei prevHeight = prevViewport[3];
+
+        glViewport(0, 0, width + 1, height + 1);
+        floor.draw();
+
+        glViewport(0, 0, prevWidth, prevHeight);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glDeleteFramebuffers(1, &frame);
+    }
+
+    [[nodiscard]] uint32_t slot() const { return m_slot; }
+    void slot(uint32_t slot) { m_slot = slot; }
+
+    void bind() const {
+        glBindTexture(GL_TEXTURE_2D, m_id);
+        glActiveTexture(GL_TEXTURE0 + m_slot);
+    }
+};
+
+
 class RayCasting final : public Simulation {
 	static void __stdcall debug_gl(
-		lwvl::debug::Source source, lwvl::debug::Type type, unsigned int id,
-		lwvl::debug::Severity severity, int length, const char* message,
-		const void* userState
+        lwvl::debug::Source source, lwvl::debug::Type type, lwvl::debug::Severity severity,
+        unsigned int id, int length, const char* message, const void* userState
 	) {
 		if (type == lwvl::debug::Type::ERROR) {
 			throw std::exception(message);
@@ -98,10 +164,10 @@ protected:
 private:
 	lwvl::debug::GLEventListener m_listener;
 	SimulationConfig settings;
-	GLuint floorTexture = 0;
 	lwvl::ShaderProgram lineShader, lightShader, floorShader;
 	std::vector<Boundary> bounds;
 	CasterConfig casters[4];
+	FloorTexture floorTexture;
 	std::unique_ptr<Floor> floor;
 	lwvl::Uniform lightCenter;
 
@@ -110,7 +176,6 @@ private:
 	}
 
 public:
-	~RayCasting() final { glDeleteTextures(1, &floorTexture); }
 	RayCasting(unsigned int width, unsigned int height, GLFWmonitor* monitor = nullptr) :
 		Simulation(width, height, "RayCasting", monitor), m_listener(this, debug_gl, true)
 	{
@@ -135,58 +200,21 @@ public:
 		const float wPad = (10.0f * frameWidth) / 800.0f;
 		const float hPad = (10.0f * frameHeight) / 600.0f;
 
-		float floorTextureWidth = frameWidth - (2.0f * wPad);
-		float floorTextureHeight = frameHeight - (2.0f * hPad);
 
-		floor = std::make_unique<Floor>(wPad, hPad, floorTextureWidth, floorTextureHeight);
+		float floorWidth = frameWidth - (2.0f * wPad);
+		float floorHeight = frameHeight - (2.0f * hPad);
 
-		// ****** Render Floor Texture ******
-		glGenTextures(1, &floorTexture); {
-			auto integerFloorWidth = static_cast<GLsizei>(floorTextureWidth);
-			auto integerFloorHeight = static_cast<GLsizei>(floorTextureHeight);
-			GLuint floorFrame;
-			glGenFramebuffers(1, &floorFrame);
-			glBindFramebuffer(GL_FRAMEBUFFER, floorFrame);
+		floorTexture.render(
+            static_cast<uint32_t>(floorWidth),
+            static_cast<uint32_t>(floorHeight)
+        );
 
-			glBindTexture(GL_TEXTURE_2D, floorTexture);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, integerFloorWidth, integerFloorHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, floorTexture, 0);
-
-			Quad textureBase(-1.0f, -1.0f, 2.0f, 2.0f);
-
-			lwvl::ShaderProgram floorTexturePipeline;
-			lwvl::VertexShader floorVertex("#version 330 core\nlayout(location=0) in vec4 position;\nvoid main() { gl_Position = position; }");
-			lwvl::FragmentShader floorFragment(readFile("Shaders/default.frag"));
-//			lwvl::FragmentShader floorFragment(readFile("Shaders/mazing.frag"));
-			floorTexturePipeline.link(floorVertex, floorFragment);
-
-			floorTexturePipeline.bind();
-
-            // Floor color
-			floorTexturePipeline.uniform("u_Color").set3f(1.00000f, 1.00000f, 1.00000f);  // White
-//			floorTexturePipeline.uniform("u_Color").set3f(0.61569f, 0.63529f, 0.67059f);  // Silver
-
-//			floorTexturePipeline.uniform("u_Resolution").set2f(floorTextureWidth, floorTextureHeight);
-
-			GLsizei prevViewport[4];
-			glGetIntegerv(GL_VIEWPORT, prevViewport);
-			glViewport(0, 0, integerFloorWidth + 1, integerFloorHeight + 1);
-			textureBase.draw();
-			glViewport(0, 0, prevViewport[2], prevViewport[3]);
-
-			glBindFramebuffer(GL_FRAMEBUFFER, 0);
-			glDeleteFramebuffers(1, &floorFrame);
-		}
+		floor = std::make_unique<Floor>(wPad, hPad, floorWidth, floorHeight);
 
 		// ****** Construct Shaders ******
-		lineShader.link({readFile("Shaders/default.vert")}, {readFile("Shaders/default.frag")});
-		lightShader.link({readFile("Shaders/light.vert")}, {readFile("Shaders/light.frag")});
-		//lightShader.link(readFile("Shaders/light.vert"), readFile("Shaders/floor.frag"));
-		floorShader.link({readFile("Shaders/floor.vert")}, {readFile("Shaders/floor.frag")});
+		lineShader.link(readFile("Shaders/default.vert"), readFile("Shaders/default.frag"));
+		lightShader.link(readFile("Shaders/light.vert"), readFile("Shaders/light.frag"));
+		floorShader.link(readFile("Shaders/floor.vert"), readFile("Shaders/floor.frag"));
 
 		// ****** Initialize Shader Uniforms ******
 		lineShader.bind();
@@ -195,16 +223,18 @@ public:
 
 		lightShader.bind();
 		lightShader.uniform("u_Projection").set2DOrthographic(frameHeight, 0.0f, frameWidth, 0.0f);
-		lightShader.uniform("u_Resolution").set2f(floorTextureWidth, floorTextureHeight);
+		lightShader.uniform("u_Resolution").set2f(floorWidth, floorHeight);
 		lightShader.uniform("u_Offset").set2f(wPad, hPad);
 //        lightShader.uniform("u_LightColor").set3f(0.05098f, 0.19608f, 0.30196f);  // Prussian Blue
-        lightShader.uniform("u_LightColor").set3f(0.30980f, 0.00392f, 0.27843f);  // Tyrian Purple
-		lightShader.uniform("u_Texture").set1i(0);
+//        lightShader.uniform("u_LightColor").set3f(0.30980f, 0.00392f, 0.27843f);  // Tyrian Purple
+        lightShader.uniform("u_LightColor").set3f(0.71373f, 0.09020f, 0.29412f);  // Pictoral Carmine
+//        lightShader.uniform("u_LightColor").set3f(0.76471f, 0.92157f, 0.47059f);  // Yellow Green Crayola
+		lightShader.uniform("u_Texture").set1i(floorTexture.slot());
 		lightCenter = lightShader.uniform("u_MouseCoords");
 
 		floorShader.bind();
 		floorShader.uniform("u_Projection").set2DOrthographic(frameHeight, 0.0f, frameWidth, 0.0f);
-		floorShader.uniform("u_Texture").set1i(0);
+		floorShader.uniform("u_Texture").set1i(floorTexture.slot());
 
 		// ****** Construct Boundaries ******
 		bounds.reserve(14); {
@@ -284,8 +314,7 @@ public:
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Draw caster.
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		floorTexture.bind();
 		floorShader.bind();
 		floor->draw();
 
@@ -294,7 +323,7 @@ public:
 
 		if (settings.config & SHOW_BOUNDS) {
 			lineShader.bind();
-			for (const Boundary& bound : bounds) {
+			for (Boundary& bound : bounds) {
 				bound.draw();
 			}
 		}
