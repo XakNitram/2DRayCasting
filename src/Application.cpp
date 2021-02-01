7f94517b2a8668cc694ddb77da6a21bcdaf57dbd
Tate Mioton <tatemioton@outlook.com>
2021-02-01 05:54:02 -0600

    Move NodeRenderer to Separate File

--- src/Application.cpp
+++ src/Application.cpp
@@ -7,88 +7,16 @@
 #include "Primitives/Boundary.hpp"
 #include "Primitives/Floor.hpp"
 #include "Primitives/FloorTexture.hpp"
+#include "Primitives/NodeRenderer.hpp"
 #include "Casters/AngleCaster.hpp"
 #include "Casters/EndPointCaster.hpp"
 
 // Code Signing: https://stackoverflow.com/questions/16673086/how-to-correctly-sign-an-executable/48244156
-// MSVCP140D
-// VCRUNTIME140D
-// ucrtbased.dll
 
 constexpr unsigned int FOLLOW_MOUSE = 0b1;
 constexpr unsigned int SHOW_BOUNDS  = 0b10;
-constexpr uint32_t CIRCLE_SLICES = 64;
+constexpr uint32_t CIRCLE_SLICES = 32;
 constexpr float M_TAU = 6.283185307179586f;
-
-
-// We can remove this template variable in the future
-//  if it makes the class easier to use.
-template<size_t capacity>
-class NodeRenderer {
-    // Attributes
-    lwvl::VertexArray vao;
-    lwvl::ArrayBuffer vbo;
-
-    std::vector<LineSegment> m_segments;
-
-    // Methods
-    std::array<float, capacity * 4> collectData() {
-        std::array<float, capacity * 4> temp {};
-
-        size_t currentElements = size();
-        for (size_t i = 0; i < currentElements; i++) {
-            LineSegment& segment = m_segments[i];
-            temp[i * 4 + 0] = segment.a.x;
-            temp[i * 4 + 1] = segment.a.y;
-            temp[i * 4 + 2] = segment.b.x;
-            temp[i * 4 + 3] = segment.b.y;
-        }
-
-        return temp;
-    }
-
-public:
-    NodeRenderer() {
-        m_segments.reserve(capacity);
-
-        vao.bind();
-        vbo.bind();
-        vbo.usage(lwvl::Usage::Dynamic);
-
-        std::array<float, capacity * 4> data = collectData();
-        vbo.construct(data.begin(), data.end());
-        vao.attribute(2, GL_FLOAT, 2 * sizeof(float), 0);
-
-        lwvl::VertexArray::clear();
-        lwvl::ArrayBuffer::clear();
-    }
-
-    const std::vector<LineSegment>& segments() { return m_segments; }
-
-    size_t size() { return m_segments.size(); }
-    size_t max() { return capacity; }
-
-    void add(LineSegment&& segment) {
-        if (size() != capacity) {
-            m_segments.push_back(segment);
-        } else {
-            throw std::exception("NodeRenderer capacity exceeded.");
-        }
-    }
-
-    // Maybe a remove method but I won't use it here.
-
-    void update() {
-        vbo.bind();
-        std::array<float, capacity * 4> data = collectData();
-        vbo.update(data.begin(), data.end());
-    }
-
-    void draw() {
-        vao.bind();
-        vao.drawArrays(GL_LINES, capacity * 2);
-    }
-};
 
 
 struct CasterConfig {
---
+++ src/Primitives/NodeRenderer.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include "pch.hpp"
+#include "Renderer/VertexArray.hpp"
+#include "Renderer/Buffer.hpp"
+#include "Math/Geometrics.hpp"
+
+
+// We can remove this template variable in the future
+//  if it makes the class easier to use.
+template<size_t capacity>
+class NodeRenderer {
+    // Attributes
+    lwvl::VertexArray vao;
+    lwvl::ArrayBuffer vbo;
+
+    std::vector<LineSegment> m_segments;
+
+    // Methods
+    std::array<float, capacity * 4> collectData() {
+        std::array<float, capacity * 4> temp {};
+
+        size_t currentElements = size();
+        for (size_t i = 0; i < currentElements; i++) {
+            LineSegment& segment = m_segments[i];
+            temp[i * 4 + 0] = segment.a.x;
+            temp[i * 4 + 1] = segment.a.y;
+            temp[i * 4 + 2] = segment.b.x;
+            temp[i * 4 + 3] = segment.b.y;
+        }
+
+        return temp;
+    }
+
+public:
+    NodeRenderer() {
+        m_segments.reserve(capacity);
+
+        vao.bind();
+        vbo.bind();
+        vbo.usage(lwvl::Usage::Dynamic);
+
+        std::array<float, capacity * 4> data = collectData();
+        vbo.construct(data.begin(), data.end());
+        vao.attribute(2, GL_FLOAT, 2 * sizeof(float), 0);
+
+        lwvl::VertexArray::clear();
+        lwvl::ArrayBuffer::clear();
+    }
+
+    const std::vector<LineSegment>& segments() { return m_segments; }
+
+    size_t size() { return m_segments.size(); }
+    size_t max() { return capacity; }
+
+    void add(LineSegment&& segment) {
+        if (size() != capacity) {
+            m_segments.push_back(segment);
+        } else {
+            throw std::exception("NodeRenderer capacity exceeded.");
+        }
+    }
+
+    // Maybe a remove method but I won't use it here.
+
+    void update() {
+        vbo.bind();
+        std::array<float, capacity * 4> data = collectData();
+        vbo.update(data.begin(), data.end());
+    }
+
+    void draw() {
+        vao.bind();
+        vao.drawArrays(GL_LINES, capacity * 2);
+    }
+};
